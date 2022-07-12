ead2cf56fef8da7637bd4bda3ce2dc659014f0e4
acadevy <rashyolat@gmail.com>
2022-07-12 14:03:29 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(5000, ()=>{
+    console.log("server is running now");
+    
+})
+
